54c6283ba77b35916ac4c27232d1ea031563f017
Dominic Garms <djgarms@gmail.com>
2020-08-31 22:45:53 +0800

    minor bugfix and raise lumen render

--- src/pages/[...index].tsx
+++ src/pages/[...index].tsx
@@ -1,22 +1,7 @@
 import pagesGetStaticProps from '../utils/initial-props/pagesGetStaticProps'
 import pagesGetStaticPaths from '../utils/initial-props/pagesGetStaticPaths'
-import { CONFIG } from 'lumen-cms-core'
 
-export { LmDefaultPage as default, CONFIG } from 'lumen-cms-core'
-
-
-CONFIG.ssrHooks.pageProps = [async (props) => {
-  console.log('inside of page props hook')
-
-  let newVar = {
-    ...props,
-    modified: {
-      test: 'hallo'
-    }
-  }
-  Object.assign(props, newVar)
-  return newVar
-}]
+export { LmDefaultPage as default } from 'lumen-cms-core'
 
 export const getStaticProps = pagesGetStaticProps
 export const getStaticPaths = pagesGetStaticPaths
