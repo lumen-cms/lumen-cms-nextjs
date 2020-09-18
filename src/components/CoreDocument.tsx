4fc81afa52b5cd1a2d6b1a32599b58f3a105b590
Dominic Garms <djgarms@gmail.com>
2020-09-18 13:48:18 +0800

    add facebook pixe

--- src/components/CoreDocument.tsx
+++ src/components/CoreDocument.tsx
@@ -13,6 +13,7 @@ type CoreDocumentProps = {
 export function LmCoreDocument({ props, isDevelopment }: CoreDocumentProps): JSX.Element {
   const { settings } = props
   const googleAnalyticsId = (settings?.setup_google_analytics) || SSR_CONFIG.GA
+  const facebookPixelId = settings?.setup_facebook_pixel
   const locale = settings?.setup_language || SSR_CONFIG.defaultLocale
 
   return (
@@ -25,6 +26,28 @@ export function LmCoreDocument({ props, isDevelopment }: CoreDocumentProps): JSX
       var StoryblokCacheVersion = '${LmStoryblokService.getCacheVersion()}';`
       }}></script>
       <NextScript />
+      {!isDevelopment && facebookPixelId && (
+        <>
+          <script dangerouslySetInnerHTML={{
+            __html: `
+          !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${facebookPixelId}');
+            fbq('track', 'PageView');
+          `
+          }} />
+          <noscript>
+            <img height="1" width="1" style={{ display: 'none' }}
+                 src={`https://www.facebook.com/tr?id=${facebookPixelId}&ev=PageView&noscript=1`} />
+          </noscript>
+        </>
+      )}
       {!isDevelopment && googleAnalyticsId && (
         <>
           <script
