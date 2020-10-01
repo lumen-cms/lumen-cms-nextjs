5185e543e4b33bbfc9924ddb982fd7393f847adb
Dominic Garms <djgarms@gmail.com>
2020-10-01 16:46:16 +0800

    update core

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
-import { LmDefaultApp } from 'lumen-cms-core'
-
+import { LmCoreComponents, LmDefaultApp } from 'lumen-cms-core'
+import { LmLazyComponents } from 'lumen-cms-core/src/components/LazyNamedComponents'
+Object.keys(LmLazyComponents).forEach((k) => (LmCoreComponents[k] = LmLazyComponents[k]))
 export default LmDefaultApp
