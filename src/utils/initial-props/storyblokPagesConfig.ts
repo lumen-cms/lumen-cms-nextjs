8c670c632af6214cf5c475e2492eb5cc9648d0eb
Dominic Garms <djgarms@gmail.com>
2020-10-02 15:35:25 +0800

    fix

--- src/utils/initial-props/storyblokPagesConfig.ts
+++ src/utils/initial-props/storyblokPagesConfig.ts
@@ -2,7 +2,7 @@ import { SSR_CONFIG } from '../ssrConfig'
 import { PageItem } from '../../typings/generated/schema'
 
 export const getAllStoriesOfProject = async (): Promise<PageItem[]> => {
-  const stories: PageItem[] = await fetch(`https://cdn-api.lumen.media/api/all-story-paths${SSR_CONFIG.rootDirectory ? '?locale=' + SSR_CONFIG.rootDirectory : ''}`)
+  const stories: PageItem[] = await fetch(`https://cdn-api.lumen.media/api/all-story-paths?token=${SSR_CONFIG.publicToken}${SSR_CONFIG.rootDirectory ? '&locale=' + SSR_CONFIG.rootDirectory : ''}`)
     .then(r => r.json())
   return stories
 }
